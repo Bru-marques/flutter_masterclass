1529d3a4780c11eb6222ea825e3dd32a9d90b95c
Bruna Marques <brmarques1108@gmail.com>
2022-10-18 15:30:59 +0100

    role of three

--- lib/Pages/class-one/rule_of_three.dart
+++ lib/Pages/class-one/rule_of_three.dart
@@ -7,7 +7,12 @@ class RoleOfTrhee extends StatefulWidget {
   State<RoleOfTrhee> createState() => _RoleOfTrheeState();
 }
 
-var input = '';
+var a = '0';
+var b = '0';
+var c = '0';
+var d = '0';
+var newValue = '';
+var result = '';
 
 class _RoleOfTrheeState extends State<RoleOfTrhee> {
   @override
@@ -19,34 +24,135 @@ class _RoleOfTrheeState extends State<RoleOfTrhee> {
   _body() {
     return (Padding(
       padding: const EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [_fields(), const Text('Está para'), _fields()],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [_fields(), const Text('Assim como'), _fields()],
-          ),
-        ],
+      child: Form(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: 80,
+                  child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        labelText: 'Valor de A',
+                      ),
+                      initialValue: '0',
+                      validator: (value) => _validator(value),
+                      onChanged: (value) {
+                        setState(() {
+                          a = value;
+                        });
+                      }),
+                ),
+                const Text('Está para'),
+                SizedBox(
+                  width: 80,
+                  child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        labelText: 'Valor de B',
+                      ),
+                      initialValue: '0',
+                      validator: (value) => _validator(value),
+                      onChanged: (value) {
+                        setState(() {
+                          b = value;
+                        });
+                      }),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: 80,
+                  child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        labelText: 'Valor de C',
+                      ),
+                      initialValue: '0',
+                      validator: (value) => _validator(value),
+                      onChanged: (value) {
+                        setState(() {
+                          c = value;
+                        });
+                      }),
+                ),
+                const Text('Assim como'),
+                SizedBox(
+                  width: 80,
+                  child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        labelText: 'Valor de D',
+                      ),
+                      initialValue: '0',
+                      validator: (value) => _validator(value),
+                      onChanged: (value) {
+                        setState(() {
+                          d = value;
+                        });
+                      }),
+                ),
+              ],
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  _handleCalculate(
+                      int.parse(a), int.parse(b), int.parse(c), int.parse(d));
+                },
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8))),
+                child: const Text('Calcular')),
+            const SizedBox(height: 12),
+            Row(
+              children: [
+                const Text(
+                  'Resultado: ',
+                  style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  result.toString(),
+                  style: const TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     ));
   }
 
-  Widget _fields() {
-    return (Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        SizedBox(
-          width: 80,
-          child: TextField(onChanged: (value) {
-            setState(() {
-              input = value;
-            });
-          }),
-        ),
-      ],
-    ));
+  _validator(String? value) {
+    setState(() {
+      newValue = value == null || value.isEmpty ? '' : value;
+    });
+    return newValue;
+  }
+
+  _handleCalculate(a, b, c, d) {
+    if (d == 0) {
+      d = (c * b) / a;
+      return result = d.toString();
+    } else if (c == 0) {
+      c = (a * d) / b;
+      return result = c.toString();
+    } else if (a == 0) {
+      a = (c * b) / d;
+      return result = c.toString();
+    } else {
+      b = (a * d) / c;
+      return result = c.toString();
+    }
   }
 }
